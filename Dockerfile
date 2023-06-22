3484f05cd9b12b6e9947ddedd771565de607eccc
Pavel Nikolaev <PNiNikolaev@sberbank.ru>
2023-06-22 15:11:35 +0300

    refactor

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM python:3.9
+USER root
 
 WORKDIR problem-solvig
-
 COPY . ./
 
 RUN pip install -r requirements.txt
 
-ENTRYPOINT ./entrypoint.sh
+ENTRYPOINT /problem-solvig/manage.py runserver 0.0.0.0:$PORT_SERVER
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,3 @@
-if [ "$DJANGO_SUPERUSER_USERNAME" ]
-then
-    python manage.py createsuperuser \
-        --noinput \
-        --username $DJANGO_SUPERUSER_USERNAME \
-        --email $DJANGO_SUPERUSER_EMAIL
-fi
-
-python manage.py mekemigrations
+python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver 0.0.0.0:$PORT_SERVER
